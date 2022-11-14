52a130b56fdbbbc57439af89b31c8e1b6206d695
MouadBNL <mouadbnl.2k01@gmail.com>
2022-11-14 20:21:31 +0100

    Installed NaiveUI

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Solving tailwind style Conflict
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
+
 createApp(App).mount('#app')
